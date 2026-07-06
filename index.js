92fefb2992b844e2ceef319018efa1a452a84b8d
Humberto Reinoso Laptop <hreinoso@gmail.com>
2026-07-06 03:53:44 +0000

    Add initial devcontainer configuration and sample Node.js script

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("------------------------------------------------");
+console.log("¡Hola desde Node.js dentro de un Dev Container!");
+console.log("------------------------------------------------");
